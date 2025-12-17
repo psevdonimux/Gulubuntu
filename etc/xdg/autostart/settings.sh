a65a8b5e90af4d474622129a1e2fb8bffb736b6f
psevdonimux <mail_451@mail.ru>
2025-12-17 23:59:43 +0300

    Первый файл сайта

--- etc/xdg/autostart/settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-performance &
-lxqt-config-monitor -l & #monitor-start
-bluefilter & #blue-filter
-xset s off -dpms & #hibernator
-okr & 
-picom &
-BACKLIGHT=$(ls /sys/class/backlight/ | head -n1) && MAX=$(cat /sys/class/backlight/$BACKLIGHT/max_brightness) && echo $MAX | sudo tee /sys/class/backlight/$BACKLIGHT/brightness &
--- usr/lib/xorg/xorg-wrapper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /usr/lib/xorg/Xorgs -logfile /dev/null "$@" > /dev/null 2>&1
